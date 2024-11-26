33d36beb92f42dac9ade9499fb1eb9b7ce2acf9f
Mathewos Worku <se.mathewos.worku@gmail.com>
2024-11-26 10:37:09 +0300

    Created an env file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded( { extended: true }))
+env.config();
 
 app.get("/", (req, res) => {
     res.send("Hello World!!!");
@@ -17,8 +19,8 @@ app.get("/get", async (req, res) => {
             bill_id: 120060
         },
         headers: {
-            "api-key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRhbWEgd2F0ZXIgc3VwcGx5IGFuZCBzZXdlcmFnZSBhdXRob3JpdHkiLCJzdWIiOiIyMTk4MjUiLCJwZXJtaXNzaW9ucyI6WyJCSUxMRVIiXSwiaXNzIjoiaHR0cHM6Ly9hcGkuZGVyYXNoLmdvdi5ldCIsImp0aSI6ImQ3ZjQyZGEwLWExNmEtMTFlOS1iZmMyLTc5ZDdmYTA1OGFjMSIsImlhdCI6MTU2MjU4MTU4N30.W2IoDlFgwIKSsyG3z5NSFryPyvKwrr41xf2UK2K3G-o",
-            "api-secret": "td4zj8rk0ro84gxnbfkk85xa0s4dx60ugphf"
+            "api-key": process.env.API_KEY,
+            "api-secret": process.env.API_SECRET
         }
     });
     console.log(result.data);
